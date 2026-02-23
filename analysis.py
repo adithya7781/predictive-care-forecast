3b50493deeb7830e9da502a73940e350530eafcb
K.Vishal adithya <138603807+adithya7781@users.noreply.github.com>
2026-02-23 12:43:48 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -113,9 +113,6 @@ def ml_forecast(train, test):
     X_train = X_train.select_dtypes(include=[np.number]).fillna(0)
     X_test = X_test.select_dtypes(include=[np.number]).fillna(0)
 
-    st.write(pd.Series(model.feature_importances_, index=X_train.columns)
-         .sort_values(ascending=False))
-
     model = RandomForestRegressor(n_estimators=200, random_state=42)
     model.fit(X_train, y_train)
 
